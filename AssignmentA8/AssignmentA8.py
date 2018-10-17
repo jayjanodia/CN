ab0bb89c44ce1ffa1befc472a481a6329e9b7a97
jayjanodia <jay.janodia@gmail.com>
2018-10-17 12:10:03 +0530

    Completed

--- AssignmentA8/AssignmentA8.py
+++ AssignmentA8/AssignmentA8.py
@@ -1,16 +1,20 @@
 import socket
+
 import os
 
 while True:
+
     try:
         choice=input('Enter Choice: \n1. Name to address\n2. Address to Name\n3. nsLookup\n')
+
         if choice=='1':
-            print('Address is : ', socket.gethostbyname(input('Enter name of host : ')))
+            print('Address is : ', socket.gethostbyname(input('\nEnter name of host : ')))
         elif choice=='2':
             print('Name is : ', socket.gethostbyaddr(input('Enter address of host : ')))  # type >>>import socket >>>help(socket.gethostbyaddr) 
         elif choice=='3':
             os.system('nslookup '+ input('Enter host name or address : '))
         else:
             break
+
     except Exception as e:
         print('Exception occured\n', e)
